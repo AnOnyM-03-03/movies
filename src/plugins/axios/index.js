ac7d00554e2e2fac327832fe6bc3e5f82691a239
Tolstov <tolstov.dmitrij42@gmail.com>
2022-11-16 23:05:44 +0400

    a separate module for sending requests and responding

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,12 +1,9 @@
 import axios from "axios";
+import interceptors from "@/plugins/axios/interceptors";
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
-  params: {
-    apikey: process.env.VUE_APP_API_KEY,
-    plot: "short",
-    i: "tt3896198",
-  },
 });
+interceptors(instance);
 
 export default instance;
