a2030050ad541c3d2f149e8c4b5a7eac79a09b4f
Tolstov <tolstov.dmitrij42@gmail.com>
2022-12-09 11:23:05 +0400

    we accept a list of movies and create a component for displaying movies

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <MoviesList :list="moviesList" />
   </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    MoviesList,
+  },
+  computed: {
+    ...mapGetters("movies", ["moviesList"]),
+  },
   methods: {
     ...mapActions("movies", ["fetchMovies"]),
   },
@@ -16,11 +23,8 @@ export default {
 </script>
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
