f85b21d61329cb4e6abfcb7e572133aa5da166ae
Tolstov <tolstov.dmitrij42@gmail.com>
2022-12-22 08:59:31 +0400

    component with pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="movies-pagination d-flex justify-content-center">
+    <BPagination
+      v-model="currentPageModel"
+      :per-page="moviesPerPage"
+      :total-rows="total"
+      prev-text="prev"
+      next-text="next"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MoviePagination",
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+  computed: {
+    currentPageModel: {
+      get() {
+        return this.currentPage;
+      },
+      set(value) {
+        this.$emit("pageChanged", value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.movies-pagination {
+  margin-top: 30px;
+}
+.movies-pagination >>> .pagination .page-item .page-link {
+  background-color: transparent;
+  font-size: 12px;
+  color: #fff;
+  box-shadow: none;
+}
+.movies-pagination >>> .pagination .page-item.active .page-link {
+  background-color: #fff;
+  border-color: #fff;
+  color: #000;
+}
+.movies-pagination >>> .pagination .page-item.disabled .page-link {
+  color: lightgray;
+}
+</style>
