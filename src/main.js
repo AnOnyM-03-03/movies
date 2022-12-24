9e1a73a5473a8d90b91ca46897b447d4398fbf9e
Tolstov <tolstov.dmitrij42@gmail.com>
2022-12-24 09:15:03 +0400

    configuring and rendering vue-router in the App

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueRouter from "vue-router";
+import router from "@/plugins/routers/routers";
 import store from "./store";
 import "./plugins/bootstrap";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
 new Vue({
   store,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
