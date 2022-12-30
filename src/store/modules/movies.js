d8e8d09f159e3701ea5ac41ba3ea3b46eb5dfe40
Tolstov <tolstov.dmitrij42@gmail.com>
2022-12-30 09:12:41 +0400

    finding a movie by index and deleting it

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -8,7 +8,7 @@ function serializeResponse(movies) {
     return acc;
   }, {});
 }
-const { MOVIES, CURRENT_PAGE } = mutations;
+const { MOVIES, CURRENT_PAGE, REMOVE_MOVIE } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -34,6 +34,9 @@ const moviesStore = {
     },
     [CURRENT_PAGE](state, value) {
       state.currentPage = value;
+    },
+    [REMOVE_MOVIE](state, index) {
+      state.top250IDs.splice(index, 1);
     },
   },
   actions: {
@@ -63,6 +66,13 @@ const moviesStore = {
     changeCurrentPage({ commit, dispatch }, page) {
       commit(CURRENT_PAGE, page);
       dispatch("fetchMovies");
+    },
+    removeMovie({ commit, dispatch, state }, id) {
+      const index = state.top250IDs.findIndex((item) => item === id);
+      if (index !== -1) {
+        commit(REMOVE_MOVIE, index);
+        dispatch("fetchMovies");
+      }
     },
   },
 };
